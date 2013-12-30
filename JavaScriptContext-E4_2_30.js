86d30f15085a87a5dcc8b3d117682e3a9c42b910
Jeff Sheffield <jeff.sheffield@gmail.com>
2013-12-30 11:50:41 -0600

    initial

---
+++ JavaScriptContext-E4_2_30.js
@@ -0,0 +1,19 @@
+
+console.log('2:30 into Context in Javascript');
+console.log('javascripts context... will be dictated by function invocation');
+
+foo = {
+    bar: function(){
+        console.log(this);
+    },
+    
+    baz: function(callback){
+        callback();
+    }
+}
+
+// function invocation
+foo.bar(); // 'this' points to object ... like in other languages
+
+// function invocation from a method.
+foo.baz(foo.bar); // the 'context' of 'this' now points to the browser's window object 
